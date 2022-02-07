9db9fbf59938f918b1701e09e94f25cf0fa46edc
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-02-07 10:42:55 +0530

    feat: made modal component

---
+++ Components/Modal/modal.js
@@ -0,0 +1,15 @@
+const openModal = document.querySelector("#open-modal");
+const modal = document.querySelector(".modal");
+const modalContainer = document.querySelector(".modal-container");
+const body = document.querySelector("body");
+openModal.addEventListener("click", () => {
+  modalContainer.classList.add("show-container");
+  modal.classList.add("show");
+  body.style.overflowY = "hidden";
+});
+document.addEventListener("click", (e) => {
+    if (e.target.id === "close-modal") {
+        modalContainer.classList.remove("show-container");
+        body.style.overflowY = "visible";
+  }
+});
